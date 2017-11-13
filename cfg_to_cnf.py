a2eb4da3df58117e52f3c6975341eada4bf1e0c7
MIKAYLA W <mikayla.whiteaker@gmail.com>
2017-11-12 18:07:49 -0800

    finished csv to dict

--- cfg_to_cnf.py
+++ cfg_to_cnf.py
@@ -4,27 +4,24 @@ class converter():
     """docstring for converter."""
     def __init__(self):
         super(converter, self).__init__()
+
+        reader = csv.reader(open('level1.csv'))
         grammar = {}
-
-        # with open('level1.csv', newline='') as csvfile:
-        #     reader = csv.DictReader(csvfile)
-        #     for row in reader:
-        #         for i in row:
-        #             print(i)
-
-        with open('level1.csv', mode='r') as infile:
-            reader = csv.reader(infile)
-            with open('level1_new.csv', mode='w') as outfile:
-                writer = csv.writer(outfile)
-                grammar = {rows[0]:rows[1] for rows in reader}
+        for row in reader:
+            key = row[0]
+            grammar[key] = row[1:]
         print(grammar)
-        #self.newStartState(grammar)
+
+        self.newStartState(grammar)
 
 
     def newStartState(self, grammar):
-        print("hello?")
-        for row in grammar:
-            print(row)
+        grammar['S0'] = ['S']
+        print(grammar)
+        self.premoteEps(self, grammar)
+
+    def premoteEps(self, grammar):
+        print("hmm...")
 
 test = converter()
-test.__init__()
+#test.__init__()
